032600e92e12d6079a4131ee536c403d8aef0ffb
barcelos2025 <barcelos32@gmail.com>
2025-05-29 14:14:39 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    }
+  }
+})
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
