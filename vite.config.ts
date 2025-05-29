d349b611eb9ec6ebefc7d429a835036758c6e732
barcelos2025 <barcelos32@gmail.com>
2025-05-29 14:24:04 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -7,27 +7,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     rollupOptions: {
       input: '/index.html',
     }
   }
 })
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
